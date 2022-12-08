826e1534dcf94db3c3b6c44794b9469594d979f2
HoldenSanchez <holdensanchezschool@gmail.com>
2022-12-08 09:41:24 -0600

    idek

--- index/index.js
+++ index/index.js
@@ -390,6 +390,13 @@ function purchase(item, times, spend) {
     vis_update()
 }
 
+window.transitionToPage = function(href) {
+    document.querySelector('body').style.opacity = 0
+    setTimeout(function() { 
+        window.location.href = href
+    }, 500)
+}
+
 document.addEventListener('DOMContentLoaded', function(event) {
     document.querySelector('body').style.opacity = 1
 })
--- index/world.js
+++ index/world.js
@@ -184,6 +184,13 @@ function explore_bot() {
     }
 }
 
+window.transitionToPage = function(href) {
+    document.querySelector('body').style.opacity = 0
+    setTimeout(function() { 
+        window.location.href = href
+    }, 500)
+}
+
 document.addEventListener('DOMContentLoaded', function(event) {
     document.querySelector('body').style.opacity = 1
 })
